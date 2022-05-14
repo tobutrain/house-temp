d8343cb1da4d63787e631303d8f86d17efa2cfb5
tobutore <tobutrain_10000@yahoo.co.jp>
2022-05-14 21:50:36 +0900

    LINE通知追加2

--- getTemp.py
+++ getTemp.py
@@ -148,7 +148,7 @@ def send_line_notify(notification_message):
     line_notify_token = 'UmQClQJvwC2MTnP3pTdl0iX3nL8Esi2XWURU7MI6lnb'
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
-    data = {'message': f'message: {notification_message}'}
+    data = {'message': f'{notification_message}'}
     requests.post(line_notify_api, headers = headers, data = data)
 
 
